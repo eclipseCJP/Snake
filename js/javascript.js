e3c02e43eb8f69324a01e9e8da98989035017fd7
eclipseCJP <cris.tecwordd@gmail.com>
2021-08-09 21:39:13 -0400

    definir movimentos da cobrinha

--- js/javascript.js
+++ js/javascript.js
@@ -6,6 +6,7 @@ snake[0]={
     x:8 * box,
     y:8 * box
 }
+let direction="right";
 
 function criarBG(){ /*desenha a imagem*/ 
     context.fillStyle ="lightgreen";
@@ -17,5 +18,22 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box,box);
     }
 }
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    let snakeX=snake[0].x;
+    let snakeY=snake[0].y;
+    if(direction=="right") snakeX +=box;
+    if(direction=="left") snakeX -= box;
+    if(direction =="up") snakeY -= box;
+    if(direction == "down") snakeY +=box;
+
+    snake.pop();
+let newHead=    {
+            x: snake,
+            y:snake
+        }
+        
+    snake.unshift(newHead);
+}
+let jogo=setInterval(iniciarJogo,100);
